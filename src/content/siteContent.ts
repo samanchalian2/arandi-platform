9d5362311af39083c40f437ec13a14fac8bbc9cb
Saman Chalian <s.chalian@PNS.local>
2026-07-26 16:10:38 +0330

    refactor: centralize content entry point

--- src/content/siteContent.ts
+++
@@ -1,2 +0,0 @@
-export { getChatContent, getCompanyContent, getFeatureContent, getFooterContent, getHeroContent, getMetadataContent, getNavigationContent } from "./index";
-export type { ChatContent, CompanyContent, FeatureContent, FooterContent, HeroContent, Language, MetadataContent, NavigationContent } from "./index";
