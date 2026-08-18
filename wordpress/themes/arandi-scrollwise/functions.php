be4f0cc4ff5ccc364e7e4f5545d874f4ba5dbffd
samanchalian2 <saman.chalian@gmail.com>
2026-08-18 18:54:06 +0330

    Add Scrollwise camera motion

--- wordpress/themes/arandi-scrollwise/functions.php
+++ wordpress/themes/arandi-scrollwise/functions.php
@@ -1,4 +1,4 @@
 <?php
 defined('ABSPATH') || exit;
 add_action('after_setup_theme',function(){register_nav_menus(array('primary'=>'منوی اصلی آرندی'));});
-add_action('wp_enqueue_scripts',function(){wp_enqueue_style('arandi-scrollwise',get_stylesheet_uri(),array('generate-style'),'1.0.0');});
+add_action('wp_enqueue_scripts',function(){wp_enqueue_style('arandi-scrollwise',get_stylesheet_uri(),array('generate-style'),'1.0.0');wp_enqueue_script('arandi-scrollwise',get_stylesheet_directory_uri().'/assets/scrollwise.js',array(), '1.0.0',true);});
