607d61c6129fc60863c9a221d299b11bcbe5a11f
samanchalian2 <saman.chalian@gmail.com>
2026-08-18 18:27:00 +0330

    Add WordPress primary navigation

--- wordpress/themes/arandi-default-enterprise/functions.php
+++ wordpress/themes/arandi-default-enterprise/functions.php
@@ -1,3 +1,4 @@
 <?php
 defined('ABSPATH') || exit;
+add_action('after_setup_theme',function(){register_nav_menus(array('primary'=>'منوی اصلی آرندی'));});
 add_action('wp_enqueue_scripts',function(){wp_enqueue_style('arandi-default-enterprise',get_stylesheet_uri(),array('generate-style'),'1.0.0');});
